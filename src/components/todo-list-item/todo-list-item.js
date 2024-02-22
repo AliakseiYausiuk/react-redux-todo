5721bda786bbd13b845b6d5d3c142db03f016084
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-02-22 16:22:29 +0300

    feat: add btn

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,11 +1,27 @@
 import React from "react";
+import "./todo-list-item.css";
 
 const TodolistItem = ({ label, important = false }) => {
-  const style = {
-    color: important ? "tomato" : "black",
-  };
+  let classNames = "todo-list-item";
+  return (
+    <span className={classNames}>
+      <span className="todo-list-item-label">{label}</span>
 
-  return <span style={style}>{label}</span>;
+      <button
+        type="button"
+        className="btn btn-outline-success btn-sm float-right"
+      >
+        <i className="fa fa-exclamation"></i>
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-outline-danger btn-sm float-right"
+      >
+        <i className="fa bi-trash"></i>
+      </button>
+    </span>
+  );
 };
 
 export default TodolistItem;
