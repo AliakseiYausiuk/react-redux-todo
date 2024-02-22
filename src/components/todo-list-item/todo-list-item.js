c03a02768bb0d14ca699dd43360ae2b15c967cd1
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-02-22 16:47:04 +0300

    fix: add classes

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,27 +1,28 @@
 import React from "react";
 import "./todo-list-item.css";
 
-const TodolistItem = ({ label, important = false }) => {
-  let classNames = "todo-list-item";
-  return (
-    <span className={classNames}>
-      <span className="todo-list-item-label">{label}</span>
+export default class TodolistItem extends React.Component {
+  render() {
+    const { label, important = false } = this.props;
+    let classNames = "todo-list-item";
+    return (
+      <span className={classNames}>
+        <span className="todo-list-item-label">{label}</span>
 
-      <button
-        type="button"
-        className="btn btn-outline-success btn-sm float-right"
-      >
-        <i className="fa fa-exclamation"></i>
-      </button>
+        <button
+          type="button"
+          className="btn btn-outline-success btn-sm float-right"
+        >
+          <i className="fa fa-exclamation"></i>
+        </button>
 
-      <button
-        type="button"
-        className="btn btn-outline-danger btn-sm float-right"
-      >
-        <i className="fa bi-trash"></i>
-      </button>
-    </span>
-  );
-};
-
-export default TodolistItem;
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+        >
+          <i className="fa bi-trash"></i>
+        </button>
+      </span>
+    );
+  }
+}
