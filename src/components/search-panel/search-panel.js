d4c27268be832fa02612cb9299287f3fab4d5e1a
AliakseiYausiuk <AliakseiYausiuk@gmail.com>
2024-02-28 13:09:22 +0300

    feat: add search

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -2,7 +2,23 @@ import React, { Component } from "react";
 import "./search-panel.css";
 
 export default class SearchPanel extends Component {
+  state = {
+    term: "",
+  };
+
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
   render() {
-    return <input placeholder="search" className="form-control search-input" />;
+    return (
+      <input
+        placeholder="search"
+        className="form-control search-input"
+        value={this.state.value}
+        onChange={this.onSearchChange}
+      />
+    );
   }
 }
